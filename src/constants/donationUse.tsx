c9630f512f3246b1e069a7394402280291b96d36
Rijwan <rijwanjaydee@gmail.com>
2025-09-09 13:27:12 +0530

    donation page

---
+++ src/constants/donationUse.tsx
@@ -0,0 +1,62 @@
+export const donationuseEN = [
+  {
+    id: 1,
+    title: "Cultural & Political Awareness",
+    image: "/images/du1.png",
+    description: "Meetings, workshops, campaigns, and public engagement",
+  },
+  {
+    id: 2,
+    title: "Publications & Advocacy",
+    image: "/images/du2.png",
+    description: "Reports, blogs, and awareness through social media",
+  },
+  {
+    id: 3,
+    title: "Support for Vulnerable Communities",
+    image: "/images/du3.png",
+    description: "Food distribution, emergency relief, and legal aid",
+  },
+];
+
+export const donationuseBN = [
+  {
+    id: 1,
+    title: "সাংস্কৃতিক ও রাজনৈতিক সচেতনতা",
+    image: "/images/du1.png",
+    description: "সভা, কর্মশালা, প্রচারণা এবং জনসম্পৃক্ততা",
+  },
+  {
+    id: 2,
+    title: "প্রকাশনা ও অধিবক্তা কার্যক্রম",
+    image: "/images/du2.png",
+    description: "প্রতিবেদন, ব্লগ এবং সামাজিক মাধ্যমে সচেতনতা বৃদ্ধি",
+  },
+  {
+    id: 3,
+    title: "ঝুঁকিপূর্ণ সম্প্রদায়ের সহায়তা",
+    image: "/images/du3.png",
+    description: "খাদ্য বিতরণ, জরুরি ত্রাণ এবং আইনি সহায়তা",
+  },
+];
+
+export const donationuseAR = [
+  {
+    id: 1,
+    title: "التوعية الثقافية والسياسية",
+    image: "/images/du1.png",
+    description: "اجتماعات، ورش عمل، حملات، ومشاركة جماهيرية",
+  },
+  {
+    id: 2,
+    title: "المنشورات والمناصرة",
+    image: "/images/du2.png",
+    description: "تقارير، مدونات، ونشر الوعي عبر وسائل التواصل الاجتماعي",
+  },
+  {
+    id: 3,
+    title: "دعم المجتمعات الضعيفة",
+    image: "/images/du3.png",
+    description: "توزيع الغذاء، الإغاثة الطارئة، والمساعدة القانونية",
+  },
+];
--- src/pages/donation/index.tsx
+++ src/pages/donation/index.tsx
@@ -6,9 +6,35 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Donation from "@/modules/donation";
+import { Container } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import NextImage from "@/hooks/NextImage";
+import {
+  donationuseEN,
+  donationuseAR,
+  donationuseBN,
+} from "@/constants/donationUse";
 
 export default function Donations() {
   const isClient = useLanguage();
+  const { i18n, t } = useTranslation();
+
+  const selectedLanguage = i18n.language;
+
+  let donationuse;
+
+  switch (selectedLanguage) {
+    case "en":
+      donationuse = donationuseEN;
+
+      break;
+    case "ar":
+      donationuse = donationuseAR;
+
+      break;
+    default:
+      donationuse = donationuseBN;
+  }
 
   if (!isClient) {
     return null;
@@ -20,21 +46,31 @@ export default function Donations() {
       </Head>
       <Header />
       <main className={styles.page}>
+        <section className={styles.banner}>
+          <Container>
+            <div className={styles.wrapper}>
+              <h1>{t("Donate")}</h1>
+              <h2>{t("Donate Des")}</h2>
+              <p>{t("Donate Des Two")}</p>
+            </div>
+          </Container>
+        </section>
         <div className={styles.payDonation}>
           <Donation />
         </div>
-        {/* <section className={styles.donation} id="key">
+        <section className={styles.donationUse}>
           <Container className={styles.container}>
             <div className={styles.head}>
-              <h2>{t("Support the Mission of Faith, Unity & Service")}</h2>
-              <p>{t("Support Des")}</p>
+              <h2>{t("How Your Donation Will Be Used")}</h2>
             </div>
             <div className={styles.row}>
-              {donations.map((category, index) => (
+              {donationuse.map((category, index) => (
                 <div key={index} className={styles.wrapper}>
-                  <div className={styles.image}>{category.icon}</div>
+                  <div className={styles.image}>
+                    <NextImage src={category.image} alt={""} />
+                  </div>
                   <div className={styles.content}>
-                    <h3>{category.name}</h3>
+                    <h3>{category.title}</h3>
                     <p>{category.description}</p>
                   </div>
                 </div>
@@ -42,17 +78,6 @@ export default function Donations() {
             </div>
           </Container>
         </section>
-        <section className={styles.contribution}>
-          <Container className={styles.container}>
-            <div className={styles.row}>
-              <div className={styles.image}>🌟</div>
-              <div className={styles.content}>
-                <h2>{t("Your Contribution Changes Lives")}</h2>
-                <p>{t("Contribution Des")}</p>
-              </div>
-            </div>
-          </Container>
-        </section> */}
       </main>
       <Footer />
     </>
